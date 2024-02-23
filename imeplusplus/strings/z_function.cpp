14244201ccac2683012b324211d2873d435b5ef8
Nicholai Dimov <ndimov@users.noreply.github.com>
2024-02-23 13:27:57 -0600

    add imeplusplus stuff

---
+++ imeplusplus/strings/z_function.cpp
@@ -0,0 +1,12 @@
+// Z-Function - O(n)
+// Array where ith element is greatest number of characters starting at i mathing the prefix
+
+vector<int> zfunction(const string& s){
+  vector<int> z (s.size());
+  for (int i = 1, l = 0, r = 0, n = s.size(); i < n; i++){
+    if (i <= r) z[i] = min(z[i-l], r - i + 1);
+    while (i + z[i] < n and s[z[i]] == s[z[i] + i]) z[i]++;
+    if (i + z[i] - 1 > r) l = i, r = i + z[i] - 1;
+  }
+  return z;
+}
